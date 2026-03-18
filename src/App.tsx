94abaa39940f984e64830d7817d8251b11d4db02
Hrndz16 <marlonhergom70@gmail.com>
2026-03-18 09:04:43 -0500

    inicio de react

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-sky-400 drop-shadow-lg">
+          ¡Tailwind funcionando!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
